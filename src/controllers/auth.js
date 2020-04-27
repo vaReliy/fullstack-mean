67985875bf45661f07a4fada70dcc784b1f24e20
Valeriy Garbuzov <valeriy@hydraglobe.com>
2020-04-28 02:28:02 +0300

    added bcryptjs, updated auth/register logic, added mongoose types

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
 module.exports.login = (request, response) => {
   const { email, password } = request.body;
   return response.status(200).json({
@@ -8,8 +11,31 @@ module.exports.login = (request, response) => {
   });
 };
 
-module.exports.register = (request, response) => {
-  return response.status(200).json({
-    login: true,
-  });
+module.exports.register = async (request, response) => {
+  const { email, password } = request.body;
+  const candidate = await User.findOne({ email: email });
+
+  if (candidate) {
+    response.status(409).json({
+      message: `The user with email ${email} has already exist!`,
+    });
+  } else {
+    const salt = bcrypt.genSaltSync(10);
+
+    const user = new User({
+      email,
+      password: bcrypt.hashSync(password, salt),
+    });
+
+    try {
+      await user.save();
+      response.status(201).json({
+        message: `The user ${email} has been created success!`,
+        data: user,
+      });
+    } catch (e) {
+      console.error(e);
+      // todo: handle error!
+    }
+  }
 };
