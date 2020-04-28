22831476bcfbd1967e2676b53e97f3f701668971
Valeriy Garbuzov <valeriy@hydraglobe.com>
2020-04-28 16:56:48 +0300

    implemented CRUD of Position

--- src/controllers/position.js
+++ src/controllers/position.js
@@ -1,23 +1,66 @@
-module.exports.get = (request, response) => {
+const errorHandler = require('../utils/errorHandler');
+const Position = require('../models/Position');
+
+module.exports.getByCategory = async (request, response) => {
+  try {
+    const targetCategoryId = request.params.categoryId;
+    const targetUserId = request.user.id;
+    const positionList = await Position.find({
+      category: targetCategoryId,
+      user: targetUserId,
+    });
+
+    response.status(200).json(positionList);
+  } catch (e) {
+    errorHandler(e);
+  }
+
   return response.status(200).json({
     getPosition: true,
   });
 };
 
-module.exports.update = (request, response) => {
-  return response.status(200).json({
-    updatePosition: true,
-  });
+module.exports.update = async (request, response) => {
+  try {
+    const position = await Position.findOneAndUpdate(
+      { _id: request.params.id },
+      { $set: request.body },
+      { new: true }, // result after update in DB
+    );
+
+    response.status(201).json(position);
+  } catch (e) {
+    errorHandler(e);
+  }
 };
 
-module.exports.delete = (request, response) => {
-  return response.status(200).json({
-    deletePosition: true,
-  });
+module.exports.delete = async (request, response) => {
+  try {
+    const targetPosition = await Position.remove({
+      _id: response.params.id,
+    });
+
+    response.status(200).json({
+      message: 'Successfully removed!',
+      data: targetPosition,
+    });
+  } catch (e) {
+    errorHandler(e);
+  }
 };
 
-module.exports.create = (request, response) => {
-  return response.status(200).json({
-    createPosition: true,
-  });
+module.exports.create = async (request, response) => {
+  try {
+    const { name, cost, category } = request.body;
+    const position = await new Position({
+      name,
+      cost,
+      category,
+      user: request.user.id,
+    }).save();
+
+    response.status(201).json(position);
+  } catch (e) {
+    errorHandler(e);
+  }
 };
--- src/routes/position.js
+++ src/routes/position.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const positionController = require('../controllers/position');
 
-// api/auth/position
-router.get('/:categoryId', positionController.get);
+// api/position
+router.get('/:categoryId', positionController.getByCategory);
 router.patch('/:id', positionController.update);
 router.post('/', positionController.create);
 router.delete('/:id', positionController.delete);
