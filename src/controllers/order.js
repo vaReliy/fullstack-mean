0d8d0b2c8a11b2bd1c43786b63c13a3c605bb76c
Valeriy Garbuzov <valeriy@hydraglobe.com>
2020-04-30 19:08:29 +0300

    implemented CRUD of Order (only C and R)

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -1,11 +1,68 @@
-module.exports.get = (request, response) => {
-  return response.status(200).json({
-    getOrder: true,
-  });
+const Order = require('../models/Order');
+const errorHandler = require('../utils/errorHandler');
+
+/**
+ * Return list of orders with pagination:
+ * request.query.skip - skip items value,
+ * request.query.take - take items count (items per page),
+ * request.query.from - take items from date (included),
+ * request.query.to - take items to date (included),
+ * @param request
+ * @param response
+ * @returns {Promise<void>}
+ */
+module.exports.get = async (request, response) => {
+  const { skip, take, from, to } = request.query;
+  const query = {
+    user: request.user.id,
+  };
+
+  if (from || to) {
+    query.date = {};
+    if (from) {
+      query.date.$gte = from;
+    }
+    if (to) {
+      query.date.$lte = to;
+    }
+  }
+
+  try {
+    const orderList = await Order
+      .find(query)
+      .skip(+skip)
+      .limit(+take);
+
+    response.status(200).json(orderList);
+  } catch (e) {
+    errorHandler(e);
+  }
 };
 
-module.exports.create = (request, response) => {
-  return response.status(200).json({
-    createOrder: true,
-  });
+/**
+ * Generate new order from order items (request.body.list)
+ * @param request
+ * @param response
+ * @returns {Promise<void>}
+ */
+module.exports.create = async (request, response) => {
+  try {
+    const userId = request.user.id;
+    const lastOrder = await Order
+      .findOne({ user: userId })
+      .sort({ date: -1 }); // newest first
+
+    const maxOrderValue = lastOrder.order || 0;
+
+    const order = new Order({
+      list: request.body.list,
+      order: maxOrderValue + 1,
+      user: userId,
+    });
+    await order.save();
+
+    response.status(200).json(order);
+  } catch (e) {
+    errorHandler(e);
+  }
 };
