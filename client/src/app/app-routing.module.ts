8631b3f85811b78b50ebec53e4a960106ae0cab7
Valeriy Garbuzov <valeriy@hydraglobe.com>
2020-05-14 22:57:50 +0300

    added auth-guard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,18 +2,19 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {LoginPageComponent} from "./login-page/login-page.component";
+import {RegisterPageComponent} from "./register-page/register-page.component";
+import {AuthGuard} from "./shared/authGuard";
 import {AuthLayoutComponent} from "./shared/layouts/auth-layout/auth-layout.component";
 import {SiteLayoutComponent} from "./shared/layouts/site-layout/site-layout.component";
-import {RegisterPageComponent} from "./register-page/register-page.component";
 
 
 const routes: Routes = [
   { path: '', component: AuthLayoutComponent, children: [
-      { path: '', pathMatch: 'full', redirectTo: 'login' },
+      // { path: '', pathMatch: 'full', redirectTo: 'login' },
       { path: 'login', component: LoginPageComponent, },
       { path: 'register', component: RegisterPageComponent, },
   ]},
-  { path: '', component: SiteLayoutComponent, children: [
+  { path: '', component: SiteLayoutComponent, canActivate: [AuthGuard], children: [
 
     ],
   },
