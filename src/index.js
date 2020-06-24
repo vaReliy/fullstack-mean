062b751a6a3220185b5650bc47b6d2cbab75acca
Valeriy Garbuzov <valeriy@hydraglobe.com>
2020-06-24 18:22:28 +0300

    fixed server port error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const app = require('./app');
+const SERVER_PORT = process.env.SERVER_PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server run on port ${process.env.SERVER_PORT || 5000}`);
+app.listen(SERVER_PORT, () => {
+  console.log(`Server run on port ${SERVER_PORT}`);
 });
