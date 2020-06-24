4e5d021d0874680aa4a7f71260ad3540294b1f7b
Valeriy Garbuzov <valeriy@hydraglobe.com>
2020-06-25 02:32:41 +0300

    cleaned project from heroku tests

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,5 @@
-import {HttpClient} from '@angular/common/http';
 import {Component, OnInit} from '@angular/core';
 
-import {environment} from '../environments/environment';
 import {AuthService} from './shared/services/auth.service';
 
 @Component({
@@ -12,7 +10,6 @@ export class AppComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private http: HttpClient,
   ) {
   }
 
@@ -20,13 +17,6 @@ export class AppComponent implements OnInit {
     const availableToken = localStorage.getItem('auth-token');
     if (availableToken) {
       this.authService.setToken(availableToken);
-    }
-
-    if (environment.production) {
-      this.http.get('assets/heroku.port').subscribe(port => {
-        // environment.apiUrl += `:${port}`;
-        console.log('environment.apiUrl', environment.apiUrl); // fixme
-      });
     }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 const app = require('./app');
-const fs = require('fs');
-const SERVER_PORT = process.env.PORT || 5000;
+const SERVER_PORT = process.env.PORT || 4202;
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server run on port ${SERVER_PORT}`);
-});
-
-fs.writeFile('./client/dist/client/assets/heroku.port', SERVER_PORT, (err, data) => {
-    if (err) {
-        return console.log(err);
-    }
 });
